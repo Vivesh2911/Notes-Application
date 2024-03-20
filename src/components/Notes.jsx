850ac300bf1a80f4ba589d29410bc9c587fc3356
Vivesh2911 <vivesh2911@gmail.com>
2024-03-20 23:50:20 +0530

    React-APp

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -86,7 +86,7 @@ export default function Notes() {
     if (id === 'notes') {
       setNotes(e.target.value);
     } else if (id === 'title') {
-      setTitle(e.target.value);
+      setTitle(e.target.value); 
     }
   }
 
